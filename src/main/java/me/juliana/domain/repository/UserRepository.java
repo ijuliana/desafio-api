6b6fbcb2d32fb8f56a3077fae3203b8102246c00
ijuliana <juliana.santosfelix@gmail.com>
2024-07-29 00:54:09 -0300

    publicação no railway

--- src/main/java/me/juliana/domain/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package me.juliana.domain.repository;
-
-import me.juliana.domain.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository <User, Long> {
-    boolean existsByAccountNumber(String accountNumber);
-}
